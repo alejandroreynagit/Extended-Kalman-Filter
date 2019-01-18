26fe5b7607415ab7ea1c68a0c340ec3bcc7eb7dc
daniel.reyna <daniel.reynatorres@gmail.com>
2019-01-18 13:54:11 -0600

    Updated tools.cpp (RMSE and Jacobian calculation) and kalman_filter.cpp (prediction and update steps)

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -2,7 +2,7 @@
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
-
+#define PI 3.14159265
 /* 
  * Please note that the Eigen library does not initialize 
  *   VectorXd or MatrixXd objects with zeros upon creation.
@@ -17,7 +17,7 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
   x_ = x_in;
   P_ = P_in;
   F_ = F_in;
-  H_ = H_in;
+  H_ = H_in; // Measurement matrix
   R_ = R_in;
   Q_ = Q_in;
 }
@@ -26,16 +26,68 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+  
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+  float rho = sqrt( px*px + py*py );
+  
+  // If rho == 0 (something right ahead), skip the update step to avoid division by zero.
+  //Avoid Division by 0
+  if (fabs(rho) < 0.0001) {
+    return; // Is this a valid assumption??????
+  } 
+
+  // compute the Jacobian matrix
+  Hj_ = tools.CalculateJacobian(x_);
+  
+  //  Lidar Measurement that will map the state vector x into Polar Coordinates,instead of H we have h(x)
+  VectorXd hx(3);
+  hx << rho, atan2( py, px ), ( px*vx + py*vy )/rho;
+
+  // Update the state using the Extended Kalman Filter equations
+  VectorXd y = z - hx;
+  
+  // Validate +- 2pi enclosing, results are 'centred'
+  if( y[1] > PI )
+    y[1] -= 2.f*PI;
+  if( y[1] < -PI )
+    y[1] += 2.f*PI;
+  
+  // Comput new estimate (new state)
+  MatrixXd Hjt = Hj_.transpose();
+  MatrixXd S = Hj_*P_*Hjt + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_*Hjt*Si;
+  x_ = x_ + ( K*y );
+  P_ = ( I_ - K*Hj_ )*P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -14,6 +14,46 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // TODO: YOUR CODE HERE
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  // check division by zero
+  if (estimations.size() == 0) {
+    cout << "Vector shape Zero" << endl;
+    return rmse;
+  }
+  
+  if (estimations.size() != ground_truth.size()) {
+    cout << "Vector should be the same size" << endl;
+    return rmse;
+  }
+  
+  // TODO: accumulate squared residuals
+  for (int i=0; i < estimations.size(); ++i) {
+    // ... your code here
+    VectorXd residual = estimations[i] - ground_truth[i];
+    
+    // coefficient-wise multiplication
+    residual = residual.array()*residual.array();
+    //cout << "residual: " << residual << endl;
+    // residual is a 4D vector, we sum each element wise!
+    rmse += residual;
+  }
+  
+  // calculate the mean
+  rmse = rmse/estimations.size(); 
+  
+  // calculate the squared root
+  rmse = rmse.array().sqrt();
+  
+  return rmse;
+  
+  
+  
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -21,4 +61,21 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+
+  // If rho == 0 (something right ahead), skip the update step to avoid division by zero.
+  if ((px*px+py*py) < 0.0001) 
+    {
+       // statements
+       cout << "Division by zero_= " ;
+       return Hj;
+    }
+  Hj_ << px/pow((pow(px,2)+pow(py,2)),0.5), py/pow((pow(px,2)+pow(py,2)),0.5), 0, 0,
+       -py/(pow(px,2)+pow(py,2)), px/(pow(px,2)+pow(py,2)), 0, 0,
+       py*(vx*py - vy*px)/pow((pow(px,2)+pow(py,2)),1.5), px*(vy*px-vx*py)/pow((pow(px,2)+pow(py,2)),1.5), px/pow((pow(px,2)+pow(py,2)),0.5),        py/pow((pow(px,2)+pow(py,2)),0.5);
+  
+  return Hj;
 }
